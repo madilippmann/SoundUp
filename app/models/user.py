8e09d8c903aa4548a3208a62af98548804e6c6fb
Madi Lippmann <madi.lippmann@gmail.com>
2022-04-12 20:26:25 +0000

    Fixed main.yml heroku flask command

--- app/models/user.py
+++ app/models/user.py
@@ -1,15 +1,20 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    name = db.Column(db.String(40), nullable=False, unique=True)
+    is_artist = db.Column(db.Boolean)
     hashed_password = db.Column(db.String(255), nullable=False)
+
+    createdAt = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updatedAt = db.Column(db.DateTime(timezone=True), server_onupdate=func.now(), server_default=func.now())
 
     @property
     def password(self):
