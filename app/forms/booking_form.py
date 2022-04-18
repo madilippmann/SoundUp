220a971197a123ffacd40819fa21b905ad69d0fd
Madi Lippmann <madi.lippmann@gmail.com>
2022-04-18 21:38:06 +0000

    I think the create booking time validations are working...

--- app/forms/booking_form.py
+++ app/forms/booking_form.py
@@ -20,7 +20,7 @@ def check_start_time_availability(form, field):
 
     for booking in artist_bookings:
         if booking.start_date_time <= start <= booking.end_date_time:
-            raise ValidationError('Selected start time conflicts with another booking')
+            raise ValidationError('Selected start time conflicts with another booking.')
 
 
 def check_end_time_availability(form, field):
@@ -33,12 +33,20 @@ def check_end_time_availability(form, field):
             raise ValidationError('Selected end time conflicts with another booking.')
 
 
+def check_time_availability(form, field):
+    artist_bookings = Booking.query.filter(Booking.artist_id == form.data['artist_id']).all()
 
+    end = datetime.fromtimestamp(form.data['end_date_time'] / 1000)
+    start = datetime.fromtimestamp(form.data['start_date_time'] / 1000)
+
+    for booking in artist_bookings:
+        if start <= booking.start_date_time and booking.end_date_time <= end:
+            raise ValidationError('Selected times conflict with another booking')
 
 
 # today = datetime.today()
 class BookingForm(FlaskForm):
     artist_id = IntegerField('artist_id', validators=[DataRequired()])
-    start_date_time = IntegerField('start_date_time', validators=[DataRequired(), check_start_time_availability])
+    start_date_time = IntegerField('start_date_time', validators=[DataRequired(), check_time_availability, check_start_time_availability])
     end_date_time = IntegerField('end_date_time', validators=[DataRequired(), check_end_time_availability])
     description = StringField('description')
