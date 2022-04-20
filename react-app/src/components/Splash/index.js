b1227d96c9878bbafa8c54dbabd97cf902e915f8
Madi Lippmann <madi.lippmann@gmail.com>
2022-04-20 00:11:44 +0000

    Images cycling

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -1,8 +1,36 @@
+import { useEffect, useState } from 'react'
+import image_1 from '../../static/splash/1.png';
+import image_2 from '../../static/splash/2.png';
+import image_3 from '../../static/splash/3.png';
+
+import './Splash.css';
 
 const Splash = () => {
+    const [image, setImage] = useState(image_3)
+    const [n, setN] = useState(3)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setN((prev) => {
+                if (prev + 1 === 4) return 1
+                else return prev + 1
+            })
+        }, 5000)
+
+        return () => clearInterval(interval);
+    }, [])
+
     return (
-        <div>
-            Splash Body
+        <div id='splash__container'>
+            {n === 1 &&
+                <img id='splash__image' src={image_1} alt='splash' />
+            }
+            {n === 2 &&
+                <img id='splash__image' src={image_2} alt='splash' />
+            }
+            {n === 3 &&
+                <img id='splash__image' src={image_3} alt='splash' />
+            }
         </div>
     )
 }
