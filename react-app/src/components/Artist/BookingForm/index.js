a14e8973a0ca9d461bc87e7642ed1cba4dbd9253
Madi Lippmann <madi.lippmann@gmail.com>
2022-04-23 00:11:35 +0000

    Fixed invalid time range bug

--- react-app/src/components/Artist/BookingForm/index.js
+++ react-app/src/components/Artist/BookingForm/index.js
@@ -35,37 +35,40 @@ const BookingForm = ({ parent }) => {
 
 
 
-    const extractDate = (dateTime) => {
-        let date = new Date(dateTime)
+    // const extractDate = (dateTime) => {
+    //     let date = new Date(dateTime)
 
-        const year = date.getFullYear()
-        const month = date.getMonth() > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`
-        const day = dateTime.split(' ')[1]
+    //     const year = date.getFullYear()
+    //     const month = date.getMonth() > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`
+    //     const day = dateTime.split(' ')[1]
 
-        return `${year}-${month}-${day}`
-    }
+    //     return `${year}-${month}-${day}`
+    // }
 
-    const extractTime = (dateTime) => {
-        const date = new Date(dateTime);
-        const hours = date.getHours() > 9 ? date.getHours() : `0${date.getHours()}`;
-        const minutes = date.getMinutes() > 9 ? date.getMinutes() : `0${date.getMinutes()}`
+    // const extractTime = (dateTime) => {
+    //     const date = new Date(dateTime);
+    //     const hours = date.getHours() > 9 ? date.getHours() : `0${date.getHours()}`;
+    //     const minutes = date.getMinutes() > 9 ? date.getMinutes() : `0${date.getMinutes()}`
 
-        return `${hours}:${minutes}`
+    //     console.log('HOURS: ', hours, ' MINUTES: ', minutes, ' DATE: ', date)
 
-    }
+    //     return `${hours}:${minutes}`
 
-    useEffect(() => {
-        if (parent.start_date_time) {
-            setDate(() => extractDate(parent.start_date_time))
-            setStartTime(() => extractTime(parent.start_date_time))
-            setEndTime(() => extractTime(parent.end_date_time))
-        }
-    }, [])
+    // }
+
+    // useEffect(() => {
+    //     if (parent.start_date_time) {
+    //         setDate(() => extractDate(parent.start_date_time))
+    //         setStartTime(() => extractTime(parent.start_date_time))
+    //         setEndTime(() => extractTime(parent.end_date_time))
+    //     }
+    // }, [])
 
 
     useEffect(() => {
         // DATE
         const adjustedDate = String(date).split(' ').slice(0, 4).join(' ');
+        console.log('START TIME / END TIME: ', startTime, endTime)
         setParsedDate(() => adjustedDate);
 
 
@@ -80,19 +83,21 @@ const BookingForm = ({ parent }) => {
                     let [hours, minutes] = adjustedStartTime[0].split(':')
                     adjustedStartTime = `00:${minutes}`
 
+
                 } else {
                     adjustedStartTime = `0${adjustedStartTime[0]}`
                 }
             } else {
-                // if (Number(adjustedStartTime[0].split(':')[0]) === 12) {
-                //     let [hours, minutes] = adjustedStartTime[0].split(':')
-                //     adjustedStartTime = `00:${minutes}`
+                if (Number(adjustedStartTime[0].split(':')[0]) === 12) {
+                    let [hours, minutes] = adjustedStartTime[0].split(':')
+                    console.log('start minutes ', minutes)
+                    adjustedStartTime = `12:${minutes}`
 
-                // } else {
-                let [hours, minutes] = adjustedStartTime[0].split(':')
-                hours = Number(hours) + 12
-                adjustedStartTime = `${hours}:${minutes}`
-                // }
+                } else {
+                    let [hours, minutes] = adjustedStartTime[0].split(':')
+                    hours = Number(hours) + 12
+                    adjustedStartTime = `${hours}:${minutes}`
+                }
             }
 
         }
@@ -104,7 +109,6 @@ const BookingForm = ({ parent }) => {
 
         if (endTime) {
             adjustedEndTime = endTime.split(' ')
-
             if (adjustedEndTime[1] === 'AM') {
                 if (Number(adjustedEndTime[0].split(':')[0]) === 12) {
                     let [hours, minutes] = adjustedEndTime[0].split(':')
@@ -113,9 +117,17 @@ const BookingForm = ({ parent }) => {
                     adjustedEndTime = `0${adjustedEndTime[0]}`
                 }
             } else {
-                let [hours, minutes] = adjustedEndTime[0].split(':')
-                hours = Number(hours) + 12
-                adjustedEndTime = `${hours}:${minutes}`
+                console.log(adjustedEndTime[0].split(':'), ' ADJUSTED')
+                if (Number(adjustedEndTime[0].split(':')[0]) === 12) {
+                    let [hours, minutes] = adjustedEndTime[0].split(':')
+                    console.log('end minutes ', minutes)
+                    adjustedEndTime = `12:${minutes}`
+
+                } else {
+                    let [hours, minutes] = adjustedEndTime[0].split(':')
+                    hours = Number(hours) + 12
+                    adjustedEndTime = `${hours}:${minutes}`
+                }
             }
         }
 
@@ -127,6 +139,8 @@ const BookingForm = ({ parent }) => {
 
 
     useEffect(() => {
+        console.log('START DATE TIME: ', startDateTime)
+        console.log('END DATE TIME: ', endDateTime)
 
         const errors = [];
         // TODO TODO TODO TODO TODO
@@ -151,6 +165,7 @@ const BookingForm = ({ parent }) => {
 
 
 
+
     const handleSubmit = async (e) => {
         e.preventDefault()
 
@@ -158,49 +173,31 @@ const BookingForm = ({ parent }) => {
 
         let res;
 
-        if (parent.start_date_time) {
-            const booking = {
-                id: parent.id,
-                artist_id: parent.id,
-                start_date_time: Date.parse(startDateTime),
-                end_date_time: Date.parse(endDateTime),
-                description,
-            }
 
-            // parent.start_date_time = startDateTime
-            // parent.end_date_time = endDateTime
-            // parent.description = description
-
-            res = await dispatch(userActions.editBooking(booking))
-            history.push('/dashboard')
-            // FIX FIX FIX navigate to other modal page rather than refreshing
-
-            window.location.reload(false)
-        } else {
-            const booking = {
-                artist_id: parent.id,
-                start_date_time: startDateTime,
-                end_date_time: endDateTime,
-                description,
-            }
-
-            res = await dispatch(userActions.createBooking(booking))
-            if (res.errors) {
-                res.errors.forEach(error => {
-                    setValidationErrors((prev) => [...prev, error.split(' : ')[1]])
-                })
-
-                return setShowErrors(true)
-            } else {
-                setDate(() => undefined)
-                setStartTime(() => undefined)
-                setEndTime(() => undefined)
-                return history.push('/dashboard')
-            }
+        const booking = {
+            artist_id: parent.id,
+            start_date_time: startDateTime,
+            end_date_time: endDateTime,
+            description,
         }
 
+        res = await dispatch(userActions.createBooking(booking))
+        if (res.errors) {
+            res.errors.forEach(error => {
+                setValidationErrors((prev) => [...prev, error.split(' : ')[1]])
+            })
 
+            return setShowErrors(true)
+        } else {
+            setDate(() => undefined)
+            setStartTime(() => undefined)
+            setEndTime(() => undefined)
+            return history.push('/dashboard')
+        }
     }
+
+
+
 
     const now = new Date();
 
@@ -209,27 +206,10 @@ const BookingForm = ({ parent }) => {
             <h3 id='booking__title'>Book Artist</h3>
             <form onSubmit={handleSubmit}>
 
-                {/* <label htmlFor='date'>Date</label>
-                <input
-                    name='date'
-                    id='date'
-                    type='date'
-                    min={`${now.getFullYear()}-${now.getMonth()}-${now.getDay() + 1}`}
-                    onChange={(e) => setDate(() => e.target.value)}
-                    value={date}
-                /> */}
                 <div id='booking__date'>
                     <DatePicker onChange={(e) => setDate(e)} selected={date} value={date} />
                 </div>
 
-                {/* <input
-                    name='startTime'
-                    id='startTime'
-                    type='time'
-                    // max={`${now.getFullYear()}-${now.getMonth()}=${now.getDay() + 1}`}
-                    onChange={(e) => setStartTime(() => e.target.value)}
-                    value={startTime}
-                /> */}
                 <div id='booking__start-time'>
                     <label htmlFor='startTime'>Start Time: </label>
                     <div className='flex-row' id='time__search-input' >
@@ -266,14 +246,7 @@ const BookingForm = ({ parent }) => {
                         </select>
                     </div>
                 </div>
-                {/* <input
-                    name='endTime'
-                    id='endTime'
-                    type='time'
-                    // min={`${now.getFullYear()}-${now.getMonth()}=${now.getDay() + 1}`}
-                    onChange={(e) => setEndTime(() => e.target.value)}
-                    value={endTime}
-                /> */}
+
                 <div id='description__container'>
                     <label htmlFor='description'>Event Description</label>
                     <textarea
@@ -298,7 +271,7 @@ const BookingForm = ({ parent }) => {
                 )}
                 <button id='booking__button'>Book Artist</button>
             </form>
-            {/* TODO TODO TODO add onClick that refs backt to booking modal component */}
+
             {parent.start_date_time && <button type='button' >Cancel</button>}
         </div>
     );
